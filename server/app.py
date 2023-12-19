8253f52ee6d41f88338e85e3d8a46a919cfb1035
Chellyyyy <20520428@gm.uit.edu.vn>
2023-12-20 03:35:20 +0700

    add watch later

--- server/app.py
+++ server/app.py
@@ -20,6 +20,12 @@ class User(db.Model):
     age = db.Column(db.Integer, nullable=True)
     address = db.Column(db.String(255), nullable=True)
     realname = db.Column(db.String(80), nullable=True)
+    watchlist = db.relationship('Watchlist', backref='user', uselist=False)
+
+class Watchlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False)  # Assuming movie_id is an integer
 
 # Thực hiện update cơ sở dữ liệu
 with app.app_context():
@@ -48,6 +54,8 @@ def register():
 
     return jsonify({'message': 'User registered successfully'})
 
+
+
 @app.route('/api/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -57,7 +65,7 @@ def login():
     if user and bcrypt.check_password_hash(user.password, data.get('password')):
         return jsonify({'message': 'Login successful'})
     else:
-        return jsonify({'message': 'Login failed'})
+        return jsonify({'message': 'Invalid username or password'}), 401
 
 @app.route('/api/update_password', methods=['POST'])
 def update_password():
@@ -69,7 +77,6 @@ def update_password():
     user = get_user_by_username(username)
 
     if user and bcrypt.check_password_hash(user.password, current_password):
-        # Update the password with the new hashed password
         hashed_new_password = bcrypt.generate_password_hash(new_password).decode('utf-8')
         user.password = hashed_new_password
 
@@ -77,6 +84,58 @@ def update_password():
         return jsonify({'message': 'Password updated successfully'})
     else:
         return jsonify({'message': 'Invalid credentials or user not found'}), 400
+
+@app.route('/api/add_remove_watchlater', methods=['POST'])
+def add_remove_watchlater():
+    data = request.get_json()
+    username = data.get('username')
+    # movie_id = data.get('movie_id')
+    user = get_user_by_username(username)
+
+    if user:
+        movie_id = data.get('movie_id')
+
+        # Kiểm tra xem movie_id đã có trong watchlist chưa
+        watchlist_entry = Watchlist.query.filter_by(user_id=user.id, movie_id=movie_id).first()
+
+        if watchlist_entry:
+            # Nếu đã tồn tại, xóa khỏi watchlist
+            db.session.delete(watchlist_entry)
+            message = 'Removed from watchlist'
+            result = False
+        else:
+            # Nếu chưa tồn tại, thêm vào watchlist
+            new_watchlist_entry = Watchlist(user_id=user.id, movie_id=movie_id)
+            db.session.add(new_watchlist_entry)
+            message = 'Added to watchlist'
+            result = True
+
+        db.session.commit()
+        response_data = {'message': message, 'result': result}
+        print("Response data:", response_data)  # Add this line for debugging
+        return jsonify(response_data)
+    else:
+        return jsonify({'message': 'User not found'}), 400
+
+    
+@app.route('/api/check_movie_in_watchlist', methods=['POST'])
+def check_movie_in_watchlist():
+    data = request.get_json()
+    username = data.get('username')
+    movie_id = data.get('movie_id')
+
+    user = get_user_by_username(username)
+
+    if user:
+        # Kiểm tra xem có entry nào trong bảng UserWatchlist với user_id và movie_id này chưa
+        watch_entry = Watchlist.query.filter_by(user_id=user.id, movie_id=movie_id).first()
+
+        if watch_entry:
+            return jsonify({'result': True})  # Movie exists in Watchlist
+        else:
+            return jsonify({'result': False})  # Movie does not exist in Watchlist
+    else:
+        return jsonify({'message': 'User not found'}), 400
 
 @app.route('/api/update_profile', methods=['POST'])
 def update_profile():
@@ -96,7 +155,6 @@ def update_profile():
     else:
         return jsonify({'message': 'User not found'}), 400
 
-
 @app.route('/api/get_user_info', methods=['POST'])
 def get_user_info():
     data = request.get_json()
@@ -115,7 +173,7 @@ def get_user_info():
         return jsonify(user_info)
     else:
         return jsonify({'message': 'User not found'}), 400
-    
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
